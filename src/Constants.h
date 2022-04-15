4c3737331994d1b2e3145617794fb4d89a8ef90d
Matěj <jehlicka@mjehlicka.eu>
2022-04-15 11:46:56 +0200

    Move SIM700G pinout definition to Constants.h

--- src/Constants.h
+++ src/Constants.h
@@ -3,6 +3,24 @@
 
 #include <string>
 
+#define SD_MISO     2
+#define SD_MOSI     15
+#define SD_SCLK     14
+#define SD_CS       13
+
+#define SerialAT    Serial1
+#define UART_BAUD   115200
+#define PIN_DTR     25
+#define PIN_TX      27
+#define PIN_RX      26
+#define PWR_PIN     4
+
+#define LED_PIN     12
+#define PIN_ADC_BAT 35
+#define PIN_ADC_SOLAR 36
+#define ADC_BATTERY_LEVEL_SAMPLES 100
+
+
 #define uS_TO_S_FACTOR 1000000
 static const std::string CONFIG_PATH = "/config.json";
 static const int DEFAULT_VOLUME = 100; // %
